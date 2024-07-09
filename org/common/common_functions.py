8e69eb1e71afd5591de4f7670fa2c13bfa3e18eb
rajasurender11 <rajasurender11@gmail.com>
2024-07-09 22:18:26 +0530

    added join

--- org/common/common_functions.py
+++ org/common/common_functions.py
@@ -1,4 +1,5 @@
 from org.config.SparkSessionConfig import spark
+from org.DataframeConstants import atm_trans_schema
 
 def read_csv_as_dataframe(loc):
     a = spark.read.format("csv") \
@@ -8,5 +9,13 @@ def read_csv_as_dataframe(loc):
 
 def read_parquet_as_dataframe(loc):
     a = spark.read.format("parquet") \
+        .load(loc)
+    return a
+
+def read_text_as_dataframe(loc):
+    a = spark.read.format("csv") \
+        .option("header",True) \
+        .option("delimiter","|") \
+        .schema(atm_trans_schema) \
         .load(loc)
     return a
--- org/process/DataframeJoins.py
+++ org/process/DataframeJoins.py
@@ -1,4 +1,5 @@
 from pyspark.sql.functions import *
+from org.config.SparkSessionConfig import spark
 
 from org.DataframeConstants import *
 from org.common.common_functions import read_csv_as_dataframe,read_text_as_dataframe
@@ -17,4 +18,38 @@ if __name__ == "__main__":
         .groupBy(col("account_no"))\
         .agg(count(col("account_no")).alias("total_transactions"),
              sum(col("trans_amount")).alias("total_trans_amt"))
-    agg_df.show()
+
+
+
+    left_joined_df = ( accounts_df.alias("a").join(atm_trans_df.alias("b"),
+                  accounts_df["account_no"] == atm_trans_df["account_no"],"left")
+                  ).select("a.account_no","bank_name",\
+                           "customer_name","trans_amount","status")\
+                   .fillna(0,["trans_amount"])\
+                   .groupBy(col("account_no"),col("customer_name")) \
+                   .agg(count(col("status")).alias("total_transactions"),\
+                         sum(col("trans_amount")).alias("total_trans_amt"))
+    left_joined_df.show(100,False)
+
+
+    accounts_df.createOrReplaceTempView("accounts_profile")
+    atm_trans_df.createOrReplaceTempView("atm_trans")
+
+    spark.sql("""
+    select account_no,customer_name,count(status) as trans_cnt,  sum(trans_amount) as total_transamt
+    from 
+    (select a.account_no,
+    customer_name,
+    status,
+    case when trans_amount is null then  0 else trans_amount end as trans_amount
+    from 
+    accounts_profile a 
+    left outer join 
+    atm_trans b 
+    on a.account_no = b.account_no)c
+    group by  account_no,customer_name
+    order by total_transamt desc
+    limit 3
+    """).show(100,False)
+
+
